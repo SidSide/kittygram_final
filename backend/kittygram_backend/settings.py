f2d399863ef4f654549982202072099a53e39fc4
Sergey_Fedosimov <Sid215@yandex.ru>
2023-06-22 22:04:12 +0300

    final

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,8 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", 'default_key')
 
 DEBUG = True
 
-# ALLOWED_HOSTS = str(os.getenv("ALLOWED_HOSTS")).split(',')
-ALLOWED_HOSTS = ['127.0.0.1', '158.160.64.194', 'localhost', 'backend', 'sidside.didns.ru']
+ALLOWED_HOSTS = str(os.getenv("ALLOWED_HOSTS")).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
